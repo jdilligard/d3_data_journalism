290d5f089f1903fcf17765d7adc38256f30ca058
jdilligard <62362043+jdilligard@users.noreply.github.com>
2020-07-26 23:55:18 -0500

    Most of the base work is done. Needs a few more things

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 
 
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 20, bottom: 30, left: 50 },
-    width = 800 - margin.left - margin.right,
-    height = 620 - margin.top - margin.bottom;
+var margin = { top: 100, right: 5, bottom: 122, left: 60 },
+    width = 1500 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
@@ -20,18 +20,16 @@ var svg = d3.select("#scatter")
 
 
 
-
-
 d3.csv('assets/data/data.csv').then(function(data) {
     console.log(data[0].healthcare);
-    var healthcare = data.map(d => d.healthcare);
-    var poverty = data.map(d => d.poverty);
+    var healthcare = parseFloat(data.map(d => d.healthcare));
+    var poverty = parseFloat(data.map(d => d.poverty));
     var abbr = data.map(d => d.abbr);
-    console.log(abbr)
+    console.log(data)
 
     // Add X axis
     var x = d3.scaleLinear()
-        .domain([0, 25])
+        .domain([8, 28])
         .range([0, width]);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -52,14 +50,62 @@ d3.csv('assets/data/data.csv').then(function(data) {
         .append("circle")
         .attr("cx", function(d) { return x(d.poverty); })
         .attr("cy", function(d) { return y(d.healthcare); })
-        .attr("r", 10)
-        .attr("stroke", "#32CD32")
+        .attr("r", 11)
+        .attr("stroke", "blue")
         .attr("stroke-width", 1.5)
-        .attr("fill", "#FFFFFF");
+        .attr("fill", "red");
 
-    var gdots = svg.selectAll("g.dot")
+
+
+
+    //
+
+    svg.selectAll("text") // Note "text", not "circle" or "rect"
         .data(data)
-        .enter().append('g');
+        .enter()
+        .append("text") // Same here!
+        .text(function(d) {
+            return d.abbr;
+        })
+        .attr("x", function(d) { return x(d.poverty) - 8; })
+        .attr("y", function(d) { return y(d.healthcare - 0.2); })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+
+
+
+    //
+    //xaxis labels
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (width / 2) + " ," +
+            (height + margin.top - 40) + ")")
+        .attr("font-size", "30px")
+        .style("text-anchor", "middle")
+        .text("My Data");
+
+    //yaxis labels
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -10 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("font-size", "30px")
+
+    .style("text-anchor", "middle")
+        .text("Value");
+
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "36px")
+        .style("text-decoration", "underline")
+        .text("Value vs Date Graph");
+
+
+
 
 
 });
